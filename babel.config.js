7937e23116c9ecc507b6ca3570809e2cc231a4a6
Mr.cong <2534069177@qq.com>
2022-12-11 17:54:11 +0800

    init

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: Mr.Cong Wei
+ * @Date: 2022-12-11 17:47:21
+ * @LastEditTime: 2022-12-11 17:47:25
+ */
+module.exports = {
+    presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript',],
+};
---
+++ src/reactivity/index.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: Mr.Cong Wei
+ * @Date: 2022-12-11 17:31:05
+ * @LastEditTime: 2022-12-11 17:40:25
+ */
+export function add(a, b) {
+    return a + b
+}
